975abff6aaf40243c31bec7e6ac28cfe28e66311
Heiko Schmidt <mail@heikoschmidt.info>
2018-06-14 20:16:56 +0200

    implemented basic structure until step 28

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Track from '../Track/Track';
+import './TrackList.css';
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+      </div>
+    );
+  }
+}
+
+export default TrackList;
